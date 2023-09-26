13970a45a29aa6fff2c19d064d7d2a510a513ce0
justarhymes <justin@dapshow.com>
2023-09-26 11:40:58 -0700

    css updates

--- components/sections/about-section.js
+++ components/sections/about-section.js
@@ -5,46 +5,53 @@ import Blob from "../content/blob";
 
 const AboutSection = () => {
   return (
-    <div className="flx">
-      <div className="flx-col">
-        <Image
-          src="/green-bean.png"
-          width={400}
-          height={470}
-          alt="President Bean"
-          placeholder="blur"
-          blurDataURL="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mNkyD9aDwADYAG11MKejAAAAABJRU5ErkJggg=="
-          unoptimized
-        />
-        <Blob title="Gene Goldstein" idNum="pres-bean">
-          <span className="highlight uppercase">President Bean</span>
-        </Blob>
+    <>
+      <div className="flx">
+        <div className="flx-col">
+          <Image
+            src="/green-bean.png"
+            width={400}
+            height={470}
+            alt="President Bean"
+            placeholder="blur"
+            blurDataURL="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mNkyD9aDwADYAG11MKejAAAAABJRU5ErkJggg=="
+            unoptimized
+          />
+          <Blob title="Gene Goldstein" idNum="pres-bean">
+            <span className="highlight uppercase">President Bean</span>
+          </Blob>
+        </div>
+        <div className="flx-col">
+          <Blob title="A Cute Lil' Animation Studio" idNum="about">
+            <p>
+              A one-stop shop for all
+              <br />
+              your animation needs!
+            </p>
+            <p>
+              We&apos;ve got over 20 years of experience, and we would
+              absolutely love to help you bring your ideas to life!
+            </p>
+          </Blob>
+          <Blob idNum="about-2">
+            <p>
+              Our specialty is creating top-notch short-form animated content
+              for online platforms like Instagram, TikTok and YouTube.
+            </p>
+            <p>
+              Whether you&apos;ve already got an established audience, or if you
+              have an original idea and want to expand your fanbase, we&apos;ll
+              help you create a consistent pipeline of amazing animated shorts!
+            </p>
+          </Blob>
+        </div>
       </div>
-      <div className="flx-col">
-        <Blob title="A Cute Lil' Animation Studio" idNum="about">
-          <p>
-            A one-stop shop for all
-            <br />
-            your animation needs!
-          </p>
-          <p>
-            We&apos;ve got over 20 years of experience, and we would absolutely
-            love to help you bring your ideas to life!
-          </p>
-        </Blob>
-        <Blob idNum="about-2">
-          <p>
-            Our specialty is creating top-notch short-form animated content for
-            online platforms like Instagram, TikTok and YouTube.
-          </p>
-          <p>
-            Whether you&apos;ve already got an established audience, or if you
-            have an original idea and want to expand your fanbase, we&apos;ll
-            help you create a consistent pipeline of amazing animated shorts!
-          </p>
-        </Blob>
+      <div className="jelly">
+        <div className="jelly-top"></div>
+        <div className="jelly-top-corner-left"></div>
+        <div className="jelly-top-corner-right"></div>
       </div>
-    </div>
+    </>
   );
 };
 
--- components/sections/services-section.js
+++ components/sections/services-section.js
@@ -1,29 +1,40 @@
-import React from 'react';
+import React from "react";
 
 import Blob from "../content/blob";
 
 const ServicesSection = () => {
   return (
-    <div className="flx container">
-      <Blob title="Services" idNum="service">
-        Thanks to our <span className="highlight">unique production pipeline</span> and <span className="highlight">broad range of world-class talent</span>, we can provide <span className="highlight">all of these services</span> for your project:
-      </Blob>
-      <Blob title="Animation" idNum="animation-services"></Blob>
-      <div>
-        <Blob title="Design" idNum="design-services"></Blob>
-        <Blob title="Storyboards" idNum="storyboards-services"></Blob>
+    <>
+      <div className="flx container">
+        <Blob title="Services" idNum="service">
+          Thanks to our{" "}
+          <span className="highlight">unique production pipeline</span> and{" "}
+          <span className="highlight">broad range of world-class talent</span>,
+          we can provide{" "}
+          <span className="highlight">all of these services</span> for your
+          project:
+        </Blob>
+        <Blob title="Animation" idNum="animation-services"></Blob>
+        <div>
+          <Blob title="Design" idNum="design-services"></Blob>
+          <Blob title="Storyboards" idNum="storyboards-services"></Blob>
+        </div>
+        <Blob title="Production" idNum="production-services"></Blob>
+        <div>
+          <Blob title="Music" idNum="music-services"></Blob>
+          <Blob title="Sound Design" idNum="sound-services"></Blob>
+        </div>
+        <Blob title="Merch" idNum="merch-services"></Blob>
+        <div>
+          <Blob title="Editing" idNum="editing-services"></Blob>
+          <Blob title="Post-Production" idNum="post-services"></Blob>
+        </div>
       </div>
-      <Blob title="Production" idNum="production-services"></Blob>
-      <div>
-        <Blob title="Music" idNum="music-services"></Blob>
-        <Blob title="Sound Design" idNum="sound-services"></Blob>
+      <div className="jelly">
+        <div className="jelly-side-left"></div>
+        <div className="jelly-side-left"></div>
       </div>
-      <Blob title="Merch" idNum="merch-services"></Blob>
-      <div>
-        <Blob title="Editing" idNum="editing-services"></Blob>
-        <Blob title="Post-Production" idNum="post-services"></Blob>
-      </div>
-    </div>
+    </>
   );
 };
 
