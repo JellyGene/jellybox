6725b69ee7b60e98ce331b096c01c9382421962e
justarhymes <justin@dapshow.com>
2023-09-25 22:55:03 -0700

    image fix

--- components/content/drop.js
+++ components/content/drop.js
@@ -1,18 +1,25 @@
-import React from 'react';
+import React from "react";
+import Image from "next/image";
 
-const Drop = ({ children, className, poster, posterAlt, idNum, title, link}) => {
+const Drop = ({
+  children,
+  className,
+  poster,
+  posterAlt,
+  idNum,
+  title,
+  link,
+}) => {
   return (
-    <section className={ `drop drop-${ idNum ?? 'normal' } ${className}` }>
+    <section className={`drop drop-${idNum ?? "normal"} ${className}`}>
       <a className="drop-content" href={link}>
         <header className="drop-title">
-          <h3>{ title }</h3>
+          <h3>{title}</h3>
         </header>
-        <div className="drop-body">
-          { children }
-        </div>
+        <div className="drop-body">{children}</div>
       </a>
       <div className="drop-image">
-        <img src={ poster } alt={ posterAlt ?? title } />
+        <Image src={poster} width={698} height={468} alt={posterAlt ?? title} />
       </div>
     </section>
   );
