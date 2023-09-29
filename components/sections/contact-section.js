6c09f93423bc270b0fdfc81f19ab095c78cc4eb4
Justin <justin@dapshow.com>
2023-09-29 00:30:25 -0700

    mobile

--- components/sections/contact-section.js
+++ components/sections/contact-section.js
@@ -4,7 +4,7 @@ import ContactForm from "../contact-form";
 
 const ContactSection = () => {
   return (
-    <div className="flx column center">
+    <div className="flx column center container">
       <h1 className="contact-title"><span className="text-hide">Contact</span></h1>
       <ContactForm />
     </div>
