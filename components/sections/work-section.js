1c1af0c8de3cc14dab6dd3a4977f68619d68e90d
Justin <justin@dapshow.com>
2023-10-07 19:21:04 -0700

    sizing

--- components/sections/work-section.js
+++ components/sections/work-section.js
@@ -16,7 +16,7 @@ const WorkSection = () => {
           <br />
           <span className="highlight large">best work</span>.
         </Blob>
-        <div className="flx spaced remove-marg">
+        <div className="flx spaced">
           <Drop
             title="Planet Panic"
             poster="/work/planet-panic-poster.png"
