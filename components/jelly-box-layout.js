473c5dd4fb1851ddb8865de013bc0d41b2b2d879
justarhymes <justin@dapshow.com>
2023-10-08 22:22:15 -0700

    some fixes

--- components/jelly-box-layout.js
+++ components/jelly-box-layout.js
@@ -3,7 +3,6 @@ import { useRouter } from "next/router";
 import ReactFullpage from "@fullpage/react-fullpage";
 import VideoBg from "reactjs-videobg";
 
-import Menu from "./menu";
 import { starCartoon, candyBean } from "./fonts/fonts";
 
 function JellyBoxLayout({ children }) {
@@ -18,17 +17,14 @@ function JellyBoxLayout({ children }) {
   return (
     <article className={candyBean.className}>
       <ReactFullpage
-        navigation={true}
         licenseKey="gplv3-license"
-        scrollingSpee={800}
+        scrollingSpeed={800}
+        scrollBar={true}
         afterLoad={(origin, destination, direction, trigger) => {
           const destinationIndex = destination.index;
           const pages = ["/", "/about", "/services", "/work", "/contact"];
 
-          if (
-            router.pathname !== "/" &&
-            origin.index === destinationIndex
-          ) {
+          if (router.pathname !== "/" && origin.index === destinationIndex) {
             const incomingSection = pages.findIndex(
               (item) => item === router.pathname
             );
@@ -42,7 +38,7 @@ function JellyBoxLayout({ children }) {
           return <ReactFullpage.Wrapper>{children}</ReactFullpage.Wrapper>;
         }}
       />
-      <VideoBg wrapperClass="reel-wrapper" videoClass="reel">
+      <VideoBg wrapperClass="reel-wrapper" videoClass="reel" playsInline>
         <VideoBg.Source src="/JellyboxReel2023.webm" type="video/webm" />
         <VideoBg.Source src="/JellyboxReel2023.mp4" type="video/mp4" />
       </VideoBg>
