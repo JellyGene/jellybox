ccd3b14066b92b5629cce49c0ad93c1311d76842
justarhymes <justin@dapshow.com>
2023-10-08 19:37:34 -0700

    socials

--- components/footer.js
+++ components/footer.js
@@ -1,36 +1,55 @@
-import React from 'react';
+import React from "react";
+import Image from "next/image";
+
+const socialGroup = [
+  {
+    social: "twitter",
+    link: "https://twitter.com/Jellybox_Studio",
+  },
+  {
+    social: "instagram",
+    link: "https://www.instagram.com/jellybox_studio/",
+  },
+  {
+    social: "youtube",
+    link: "https://www.youtube.com/@genegoldstein",
+  },
+  {
+    social: "discord",
+    link: "https://discord.gg/qJEejd2Hna",
+  },
+  {
+    social: "tiktok",
+    link: "https://www.tiktok.com/@genegoldstein",
+  },
+];
+
+const SocialList = () => {
+  return (
+    <ul>
+      {socialGroup.map((object, index) => {
+        return (
+          <li key={index}>
+            <a title={`link to ${object.social}`} href={object.link}>
+              <Image
+                alt={object.social}
+                width={40}
+                height={40}
+                src={`/socials/${object.social}.png`}
+              />
+            </a>
+          </li>
+        );
+      })}
+    </ul>
+  );
+};
 
 const Footer = ({ children }) => {
-
-  const socialGroup = {
-    twitter: {
-      icon: "",
-      link:"http://google.com"
-    },
-    instagram: {
-      icon: "",
-      link:"http://google.com"
-    },
-    youtube: {
-      icon: "",
-      link:"http://google.com"
-    },
-    discord: {
-      icon: "",
-      link:"http://google.com"
-    },
-    tiktok: {
-      icon: "",
-      link:"http://google.com"
-    }
-  };
-
   return (
     <footer className="section section-footer fp-auto-height">
-      <div>
-        <ul>
-          <li></li>
-        </ul>
+      <div className="flx column center">
+        <SocialList />
         <div>
           <small>&copy; Jellybox 2023</small>
         </div>
