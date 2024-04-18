5bb3d789027907ff4f7aecc4a5ca458b089cb1b8
justarhymes <justin@dapshow.com>
2024-04-17 17:06:35 -0700

    remove contact email

--- components/sections/contact-section.js
+++ components/sections/contact-section.js
@@ -2,13 +2,6 @@ import React from "react";
 import BubbleCanvas from "../bubbles-canvas";
 import ContactForm from "../contact-form";
 
-const style = {
-  backgroundColor: "#fff",
-  borderRadius: "20px",
-  fontSize: "18px",
-  padding: "10px 15px",
-};
-
 const ContactSection = () => {
   return (
     <>
@@ -16,9 +9,6 @@ const ContactSection = () => {
         <h1 className="contact-title">
           <span className="text-hide">Contact</span>
         </h1>
-        <div className="email" style={style}>
-          business@jellybox.studio
-        </div>
         <ContactForm />
       </div>
       <BubbleCanvas />
